f2c7b8900c3a6014682f0d92a8dddf3d001a692d
gloomysw <sw.gloomysw@gmail.com>
2016-07-15 14:09:29 +0800

    首页增加用户区域点击弹框

--- content/js/home.js
+++ content/js/home.js
@@ -8,3 +8,32 @@
  * 创建时间:2016年7月14日14:38:35
  */
 $("body>.leftArea").mCustomScrollbar();
+
+/**
+ * 用户名称点击事件
+ * 创建人:邵炜
+ * 创建时间:2016年7月15日13:59:32
+ */
+$(".userArea>.userName").click(function(e){
+    $(".userAreaBox").toggle();
+    return false;
+});
+
+/**
+ * 退出按钮点击事件
+ * 创建人:邵炜
+ * 创建时间:2016年7月15日14:00:39
+ */
+$(".outBtn").click(function(){
+    $(".userAreaBox").toggle();
+    return false;
+});
+
+/**
+ * 关闭用户框事件
+ * 创建人:邵炜
+ * 创建时间:2016年7月15日14:08:42
+ */
+$(":not(.userAreaBox):not(.userName)").click(function(){
+    $(".userAreaBox").hide();
+});
